594b4d8be16617c8d5a64573c447272aa183e1a1
DavideBri <70480082+DavideBri@users.noreply.github.com>
2022-12-04 12:54:35 +0100

    Update GameResult.tsx

--- src/components/player/GameResult.tsx
+++ src/components/player/GameResult.tsx
@@ -171,13 +171,13 @@ function GameResult({ songConfig }: { songConfig: any }) {
           </div>
         </div>
         <div>
-        {/* <div className="text-center pb-3">
+        <div className="text-center pb-3">
               <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none rounded-full flex inline-flex" role="alert">
                 <span className="rounded-full bg-rose-600 uppercase px-2 py-1 text-xs font-bold mr-3">New!</span>
                 <span className="font-semibold mr-2 text-left flex-auto"><a href="http://indie.heardle.it/" >Prova Heardle Italia INDIE! </a></span>
                 <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
               </div>
-          </div> */}
+          </div>
           <NextTimer />
           <div className="items-center flex flex-col justify-center pb-3" style={{width:"100%"}}>
             <button className="px-2 py-2 uppercase tracking-widest border-none rounded-full flex items-center font-semibold mt-3 text-sm bg-slate-800">
